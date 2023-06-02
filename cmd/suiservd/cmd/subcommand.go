fe81fd701b73c1af6211d9e80ef4236e80489282
Ducca <64695363+icohigh@users.noreply.github.com>
2023-06-02 18:49:15 +0300

    add withdraw function

--- cmd/suiservd/cmd/subcommand.go
+++ cmd/suiservd/cmd/subcommand.go
@@ -7,13 +7,24 @@ import (
 )
 
 func init() {
-	RootCmd.AddCommand(subCommand)
+	RootCmd.AddCommand(mergecoin)
+	RootCmd.AddCommand(withdraw)
 }
 
-var subCommand = &cobra.Command{
+var mergecoin = &cobra.Command{
 	Use:   "merge-coins",
 	Short: "Merging all sui::SUI objects to PRIMARY_COIN",
 	Long:  "Merging all sui::SUI objects to PRIMARY_COIN",
+	Run: func(cmd *cobra.Command, args []string) {
+		// Behavior when the subcommand is executed
+		fmt.Println("Executing subcommand...")
+	},
+}
+
+var withdraw = &cobra.Command{
+	Use:   "withdraw",
+	Short: "Withdrawing all sui::SuiStaked objects",
+	Long:  "Withdrawing all sui::SuiStaked objects",
 	Run: func(cmd *cobra.Command, args []string) {
 		// Behavior when the subcommand is executed
 		fmt.Println("Executing subcommand...")
