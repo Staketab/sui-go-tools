1f58de2dd5e0ec9fd6191008a68e0cd2d24833b1
Ducca <64695363+icohigh@users.noreply.github.com>
2023-06-02 19:03:46 +0300

    add version command in json

--- cmd/suiservd/cmd/subcommand.go
+++ cmd/suiservd/cmd/subcommand.go
@@ -1,17 +1,19 @@
 package cmd
 
 import (
+	"encoding/json"
 	"fmt"
 
 	"github.com/spf13/cobra"
 )
 
 func init() {
-	RootCmd.AddCommand(mergecoin)
-	RootCmd.AddCommand(withdraw)
+	RootCmd.AddCommand(mergecoinCommand)
+	RootCmd.AddCommand(withdrawCommand)
+	RootCmd.AddCommand(versionCommand)
 }
 
-var mergecoin = &cobra.Command{
+var mergecoinCommand = &cobra.Command{
 	Use:   "merge-coins",
 	Short: "Merging all sui::SUI objects to PRIMARY_COIN",
 	Long:  "Merging all sui::SUI objects to PRIMARY_COIN",
@@ -21,12 +23,33 @@ var mergecoin = &cobra.Command{
 	},
 }
 
-var withdraw = &cobra.Command{
+var withdrawCommand = &cobra.Command{
 	Use:   "withdraw",
 	Short: "Withdrawing all sui::SuiStaked objects",
 	Long:  "Withdrawing all sui::SuiStaked objects",
 	Run: func(cmd *cobra.Command, args []string) {
 		// Behavior when the subcommand is executed
 		fmt.Println("Executing subcommand...")
+	},
+}
+
+var versionCommand = &cobra.Command{
+	Use:   "version",
+	Short: "Print the CLI version",
+	Long:  "Print the CLI version",
+	Run: func(cmd *cobra.Command, args []string) {
+		data := struct {
+			Version string `json:"version"`
+		}{
+			Version: version,
+		}
+
+		jsonData, err := json.Marshal(data)
+		if err != nil {
+			fmt.Println("Error:", err)
+			return
+		}
+
+		fmt.Println(string(jsonData))
 	},
 }
