356fd2504763fc54ee057bd05d6c51c8440be9d3
Ducca <64695363+icohigh@users.noreply.github.com>
2023-06-02 19:03:36 +0300

    add vars

---
+++ cmd/suiservd/cmd/vars.go
@@ -0,0 +1,8 @@
+package cmd
+
+var (
+	// infoLog  = log.New(os.Stdout, "INFO: ", log.Ldate|log.Ltime)
+	// errorLog = log.New(os.Stderr, "ERROR: ", log.Ldate|log.Ltime|log.Lshortfile)
+	binary  = "suiservd"
+	version = "v1.0.0"
+)
