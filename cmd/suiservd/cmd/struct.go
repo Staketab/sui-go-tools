179bf8dee13c9ff853c9fbb9312e66479c5ce300
Ducca <64695363+icohigh@users.noreply.github.com>
2023-06-03 12:14:34 +0300

    Update struct.go

--- cmd/suiservd/cmd/struct.go
+++ cmd/suiservd/cmd/struct.go
@@ -6,7 +6,7 @@ type Config struct {
 
 type DatabaseConfig struct {
 	Rpc           string `toml:"rpc"`
-	SuiBinaryPath int    `toml:"sui_binary_path"`
+	SuiBinaryPath string `toml:"sui_binary_path"`
 	Address       string `toml:"address"`
 	GasObjToPay   string `toml:"gas_odject_to_pay"`
 	PrimaryCoin   string `toml:"primary_coin"`
